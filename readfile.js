832eead512b5abbb66cedc06e85183cb4a3cf3a9
sase <bs.saase@gmail.com>
2023-04-15 12:22:44 -0500

    Created a function to read a file and return a string with the content

---
+++ readfile.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+
+export default async (filePath) => {
+  try {
+
+    return await new Promise((resolve, reject) => {
+        fs.readFile(filePath, "utf-8", (error, data) => {
+        if (error) reject(error);
+        else resolve(data);
+
+      });
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+};
